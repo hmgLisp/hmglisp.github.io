de91be85d43b9ad6a428fb8eefb59f75c9549f39
hmg <hmglisp@gmail.com>
2018-06-18 12:58:10 +0900

    aa

--- game_dev/1806_game_02/mapEdit/editor.js
+++ game_dev/1806_game_02/mapEdit/editor.js
@@ -2,7 +2,8 @@ window.addEventListener('load', editorMain, false);
 
 function editorMain () {
     var canvas = document.querySelector('#editor');
-    var ctx = canvas.getContext2d('2d');
+    //var canvas = document.getElementById('editor');
+    var ctx = canvas.getContext('2d');
 
     function draw () {
         ctx.beginPath();
