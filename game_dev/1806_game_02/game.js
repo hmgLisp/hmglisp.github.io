a738e7e210031d4f20894f9a337e03158f1a5673
hmg <hmglisp@gmail.com>
2018-06-21 12:53:31 +0900

    a

--- game_dev/1806_game_02/game.js
+++ game_dev/1806_game_02/game.js
@@ -23,8 +23,8 @@ function main() {
     }
 
     function run() {
-        var playArea = {minX: 0, minY: 0, maxX: canvas.clientWidth, maxY: canvas.clientHeight};
-        var tank = {x:canvas.clientWidth/2, y:canvas.clientHeight/2, w:30, h:30, forword:{x:0, y:-1}, moveSpeed:2};
+        let playArea = {minX: 0, minY: 0, maxX: canvas.clientWidth, maxY: canvas.clientHeight};
+        let tank = {x:canvas.clientWidth/2, y:canvas.clientHeight/2, w:30, h:30, forword:{x:0, y:-1}, moveSpeed:2, barrelLen: 30, muzzlePos: {x:0, y:0}};        
         
         //gridMap map
         var rowCount = 20;
@@ -39,9 +39,43 @@ function main() {
             for (var r = 0; r < rowCount; r++) {
                 gridMap[c][r] = {x: (c * cellWidth), y: (r * cellHeight), state: 0};            
             }
-        }    
+        }  
+
+        function turnTank() {
+            if(inputKeys[37]) {
+                tank.forword = {x: -1, y: 0};
+            }
+            else if(inputKeys[38]) {
+                tank.forword = {x: 0, y: -1};
+            } 
+            else if(inputKeys[39]) {
+                tank.forword = {x: 1, y: 0};
+            }
+            else if(inputKeys[40]) {
+                tank.forword = {x: 0, y: 1};
+            }
+        }
         
-        function drawgridMap () {        
+        function moveTank() {          
+            let dx = tank.x + tank.forword.x * tank.moveSpeed;
+            let dy = tank.y + tank.forword.y * tank.moveSpeed;
+
+            if(dx > playArea.minX && dx + tank.w < playArea.maxX
+            && dy > playArea.minY && dy + tank.h < playArea.maxY) {
+                tank.x = dx;
+                tank.y = dy;
+
+                tank.muzzlePos.x = (tank.x + tank.w / 2) + (tank.forword.x * tank.barrelLen);
+                tank.muzzlePos.y = (tank.y + tank.h / 2) + (tank.forword.y * tank.barrelLen);
+            }
+        }
+    
+        function update() {    
+            turnTank();
+            moveTank();
+        }
+
+        function drawgridMap() {        
             for (let c = 0; c < columnCount; c++) {
                 for (let r = 0; r < rowCount; r++) {
                     ctx.beginPath();
@@ -51,47 +85,31 @@ function main() {
                     ctx.stroke();
                     ctx.closePath();            
                 }
-            }    
-            
+            }                
         }
-    
-        function update () {    
-            if(inputKeys[37]) {
-                tank.forword = {x: -tank.moveSpeed, y: 0};
-            }
-            else if(inputKeys[38]) {
-                tank.forword = {x: 0, y: -tank.moveSpeed};
-            } 
-            else if(inputKeys[39]) {
-                tank.forword = {x: tank.moveSpeed, y: 0};
-            }
-            else if(inputKeys[40]) {
-                tank.forword = {x: 0, y: tank.moveSpeed};
-            }
-            
-            let dx = tank.x + tank.forword.x;
-            let dy = tank.y + tank.forword.y;
 
-            if(dx > playArea.minX && dx + tank.w < playArea.maxX
-            && dy > playArea.minY && dy + tank.h < playArea.maxY) {
-                tank.x = dx;
-                tank.y = dy;
-            }
-        }
-    
-        function render() {
-            ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-            drawgridMap();     
+        function drawTank() {            
             ctx.beginPath();
             ctx.rect(tank.x, tank.y, tank.w, tank.h);
             ctx.fillStyle = "rgb(0, 149, 221)";
             ctx.fill();
-            ctx.closePath();       
-            
-            
+            ctx.closePath();
+
+            //draw muzzle
+            ctx.beginPath();
+            ctx.arc(tank.muzzlePos.x, tank.muzzlePos.y, 5, 0, Math.PI * 2);
+            ctx.fillStyle = "rgb(0, 149, 221)";
+            ctx.fill();
+            ctx.closePath();
+        }
+    
+        function render() {
+            ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+            drawgridMap();   
+            drawTank();            
         }
         
-        function gameLoop () {
+        function gameLoop() {
             update();
             render();
         }
