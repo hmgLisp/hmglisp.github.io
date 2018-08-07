ae2380da770f100473208ec0b38c2447b29882e1
hmg <hmglisp@gmail.com>
2018-08-07 22:50:25 +0900

    aa

--- game_dev/1807_01/main.js
+++ game_dev/1807_01/main.js
@@ -1,21 +1,19 @@
 let tnak;
-let tank2;
 
 function init() {
     //canvas.setAttribute('height', '100');
     set_background("rgb(0,0,0)");
-    tank = new Tank(300,300,30,30, "rgb(100,100,100)");
-    tank2 = new Tank(400,300, 40, 40, "rgb(255, 0, 0)");
+    tank = new Tank(300,300,30,30, "#00dd1d");    
 }
 
 function update() {
-    tank.update();
-    tank2.update();
+    tank.update();    
 }
 
 class Tank extends game_obj{
     constructor(x, y, w, h, color) {
-        super(x, y, w, h);       
+        super(x, y, w, h);  
+        this.move_speed = 3;     
         this.color = color; 
     }
 
@@ -24,16 +22,16 @@ class Tank extends game_obj{
         let dy = this.y;
 
         if(input_manager.is_down_key(37)) {
-            dx -= 3;
+            dx -= this.move_speed;
         }
         if(input_manager.is_down_key(39)) {
-            dx += 3;
+            dx += this.move_speed;
         }
         if(input_manager.is_down_key(38)) {
-            dy -= 3;
+            dy -= this.move_speed;
         }
         if(input_manager.is_down_key(40)) {
-            dy +=3;
+            dy +=this.move_speed;
         }
         this.x = dx;
         this.y = dy;
