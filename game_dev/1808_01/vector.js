4cfe7626da93010c932529bee98c814502819380
hmg <hmglisp@gmail.com>
2018-08-18 14:13:43 +0900

    z

--- game_dev/1808_01/vector.js
+++ game_dev/1808_01/vector.js
@@ -4,4 +4,15 @@ class Vector {
         this.y = y;
         this.z = z;
     }
+
+    static dot(vec1, vec2) {
+        return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+    }
+
+    static cross(vec1, vec2) {
+        return new Vector(
+            vec1.y * vec2.z - vec1.z * vec2.y,
+            vec1.z * vec2.x - vec1.x * vec2.z,
+            vec1.x * vec2.y - vec1.y * vec2.x);
+    }
 }
